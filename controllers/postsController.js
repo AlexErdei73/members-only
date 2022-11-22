30bf6b388936600a9f305a166ddf0a837c8ee6b6
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-11-22 13:27:22 +0000

    Slightly change color scheeme

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,9 +1,9 @@
 const Post = require("../models/post");
 
-exports.posts_get = function(req, res, next) {
-    if (!req.user) {
-        res.redirect("/users/login");
-        return next({ status: 401, msg: "Unauthorised" });
-    }
-    res.send(`Welcome ${req.user.name}`);
-}
+exports.posts_get = function (req, res, next) {
+  if (!req.user) {
+    res.redirect("/users/login");
+    return next({ status: 401, msg: "Unauthorised" });
+  }
+  res.render("posts", { title: "Posts", message: `Welcome ${req.user.name}` });
+};
