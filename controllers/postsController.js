041805987af6d91b7a3800a0e53b073843f30336
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-11-30 14:29:49 +0000

    Make admin able to delete posts

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,31 +3,31 @@ const { body, validationResult } = require("express-validator");
 
 exports.posts_get = function (req, res, next) {
   Post.find({})
-    .sort({"createdAt": "descending"})
+    .sort({ createdAt: "descending" })
     .populate("author")
     .exec((err, posts) => {
-    if (err) {
-      return next(err);
-    }
-    res.render("posts", { 
-      title: "",
-      posts: posts,
+      if (err) {
+        return next(err);
+      }
+      res.render("posts", {
+        title: "",
+        posts: posts,
+      });
     });
-  })
 };
 
-exports.create_post_get = function(req, res, next) {
+exports.create_post_get = function (req, res, next) {
   if (!req.user) {
     res.redirect("/users/login");
     return;
   }
-  res.render("create_post", { 
-    title: "Create Post", 
+  res.render("create_post", {
+    title: "Create Post",
     form_title: "",
     youtube_url: "",
     description: "",
   });
-}
+};
 
 exports.create_post_post = [
   body("title")
@@ -40,18 +40,20 @@ exports.create_post_post = [
   body("youtube_url")
     .isURL()
     .matches(/^(https?\:\/\/)?(www\.youtube\.com|youtu\.be)\/.+$/)
-    .withMessage("You must copy the YouTube url from the address bar of the browser"),
+    .withMessage(
+      "You must copy the YouTube url from the address bar of the browser"
+    ),
   body("description")
     .optional({ checkFalsy: true })
     .trim()
     .custom((value) => value.indexOf("'") === -1)
     .withMessage("Description cannot contain apstrophe")
     .escape(),
-  function(req, res, next) {
+  function (req, res, next) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       res.locals.errors = errors.array();
-      res.render("create_post", { 
+      res.render("create_post", {
         title: "Create Post",
         form_title: req.body.title,
         youtube_url: req.body.youtube_url,
@@ -66,18 +68,18 @@ exports.create_post_post = [
     new Post({
       title: req.body.title,
       youtube_url: req.body.youtube_url,
-      description: req.body. description,
+      description: req.body.description,
       author: req.user ? req.user._id : null,
     }).save((err) => {
       if (err) {
         return next(err);
       }
       res.redirect("/posts");
-    })
-  }
-]
+    });
+  },
+];
 
-exports.like_post_get = function(req, res, next) {
+exports.like_post_get = function (req, res, next) {
   if (!req.user) {
     res.redirect("/users/login");
   }
@@ -90,15 +92,31 @@ exports.like_post_get = function(req, res, next) {
       likes.push(req.user._id);
     } else {
       likes.splice(likes.indexOf(req.user._id), 1);
-    };
-    post.update({
-      _id: req.params.id,
-      likes: likes,
-    }, (err) => {
-      if (err) {
-        return next(err);
+    }
+    post.update(
+      {
+        _id: req.params.id,
+        likes: likes,
+      },
+      (err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/posts");
       }
-      res.redirect("/posts");
-    })
-  })
-}
+    );
+  });
+};
+
+exports.delete_post_get = function (req, res, next) {
+  if (!req.user || req.user.membership_status !== "admin") {
+    res.redirect("/users/login");
+    return;
+  }
+  Post.findByIdAndDelete(req.params.id, {}, (err, post) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/posts");
+  });
+};
--- routes/posts.js
+++ routes/posts.js
@@ -10,4 +10,6 @@ router.post("/create", posts_controller.create_post_post);
 
 router.get("/like/:id", posts_controller.like_post_get);
 
+router.get("/delete/:id", posts_controller.delete_post_get);
+
 module.exports = router;
