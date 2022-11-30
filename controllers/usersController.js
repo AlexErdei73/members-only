66dd5e00e39393ddbc672b4fc8121f7841fba8da
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-11-30 12:25:19 +0000

    Make users able to get admin status

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,124 +4,137 @@ const User = require("../models/user");
 require("dotenv").config();
 
 exports.login_get = function (req, res, next) {
-	res.render("index", { title: "Log In" });
+  res.render("index", { title: "Log In" });
 };
 
 exports.logout_get = function (req, res, next) {
-	if (!req.user) {
-		res.redirect("/users/login", { title: "Log In" });
-		return;
-	}
-	req.logout((err) => {
-		if (err) {
-			return next(err);
-		}
-		res.redirect("/");
-	});
+  if (!req.user) {
+    res.redirect("/users/login", { title: "Log In" });
+    return;
+  }
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 };
 
 exports.signup_get = function (req, res, next) {
-	res.render("signup", {
-		title: "Sign Up",
-		first_name: "",
-		last_name: "",
-		user_name: "",
-		password: "",
-		confirm_password: "",
-	});
+  res.render("signup", {
+    title: "Sign Up",
+    first_name: "",
+    last_name: "",
+    user_name: "",
+    password: "",
+    confirm_password: "",
+  });
 };
 
 exports.signup_post = [
-	body("first_name")
-		.trim()
-		.isLength({ min: 1 })
-		.escape()
-		.withMessage("First Name must be specified")
-		.isAlphanumeric()
-		.withMessage("First Name can only contain alphanumeric characters"),
-	body("last_name")
-		.optional({ checkFalsy: true })
-		.trim()
-		.escape()
-		.isAlphanumeric()
-		.withMessage("Last Name can only contain alphanumeric characters"),
-	body("user_name")
-		.trim()
-		.isLength({ min: 1 })
-		.escape()
-		.withMessage("User Name must be specified")
-		.isAlphanumeric()
-		.withMessage("User Name can only contain alphanumeric characters"),
-	body("password")
-		.isLength({ min: 1 })
-		.withMessage("Password must be specified"),
-	body("confirm_password")
-		.isLength({ min: 1 })
-		.withMessage("Confirm Password must be specified")
-		.custom((value, { req }) => value === req.body.password)
-		.withMessage("Confirm Password must be the same as Password"),
-	(req, res, next) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			res.locals.errors = errors.array();
-			res.render("signup", {
-				title: "Sign Up",
-				user_name: req.body.user_name,
-				first_name: req.body.first_name,
-				last_name: req.body.last_name,
-				password: req.body.password,
-				confirm_password: req.body.confirm_password,
-			});
-			return;
-		}
-		bcrypt.hash(req.body.password, 10, (err, hash) => {
-			if (err) {
-				return next(err);
-			}
-			new User({
-				first_name: req.body.first_name,
-				last_name: req.body.last_name,
-				user_name: req.body.user_name,
-				hash: hash,
-				membership_status: "user",
-			}).save((err) => {
-				if (err) {
-					return next(err);
-				}
-				res.redirect("/");
-			});
-		});
-	},
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First Name must be specified")
+    .isAlphanumeric()
+    .withMessage("First Name can only contain alphanumeric characters"),
+  body("last_name")
+    .optional({ checkFalsy: true })
+    .trim()
+    .escape()
+    .isAlphanumeric()
+    .withMessage("Last Name can only contain alphanumeric characters"),
+  body("user_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("User Name must be specified")
+    .isAlphanumeric()
+    .withMessage("User Name can only contain alphanumeric characters"),
+  body("password")
+    .isLength({ min: 1 })
+    .withMessage("Password must be specified"),
+  body("confirm_password")
+    .isLength({ min: 1 })
+    .withMessage("Confirm Password must be specified")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Confirm Password must be the same as Password"),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.locals.errors = errors.array();
+      res.render("signup", {
+        title: "Sign Up",
+        user_name: req.body.user_name,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        password: req.body.password,
+        confirm_password: req.body.confirm_password,
+      });
+      return;
+    }
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+      new User({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        user_name: req.body.user_name,
+        hash: hash,
+        membership_status: "user",
+      }).save((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect("/");
+      });
+    });
+  },
 ];
 
+function giveMembershipStatus(password, id, status, cb) {
+  let passwordHash = process.env.MEMBER_PASSWORD;
+  if (status === "admin") passwordHash = process.env.ADMIN_PASSWORD;
+  bcrypt.compare(password, passwordHash, (err, success) => {
+    if (err) {
+      cb(err, null);
+    }
+    if (success) {
+      User.findByIdAndUpdate(id, { membership_status: status }, {}, (err) => {
+        if (err) {
+          cb(err, null);
+        }
+        cb(null, true);
+      });
+    } else {
+      cb(null, false);
+    }
+  });
+}
+
 exports.membership_post = function (req, res, next) {
-	if (!req.user) {
-		res.redirect("/users/login", { title: "Log In" });
-		return;
-	}
-	const id = req.user._id;
-	bcrypt.compare(
-		req.body.member_password,
-		process.env.MEMBER_PASSWORD,
-		(err, success) => {
-			if (err) {
-				return next(err);
-			}
-			if (success) {
-				User.findByIdAndUpdate(
-					id,
-					{ membership_status: "member" },
-					{},
-					(err) => {
-						if (err) {
-							return next(err);
-						}
-						res.redirect("/posts");
-					}
-				);
-			} else {
-				res.redirect("/posts");
-			}
-		}
-	);
+  if (!req.user) {
+    res.redirect("/users/login", { title: "Log In" });
+    return;
+  }
+  giveMembershipStatus(
+    req.body.password,
+    req.user.id,
+    "member",
+    (err, success) => {
+      if (err) {
+        return next(err);
+      }
+      if (!success)
+        giveMembershipStatus(req.body.password, req.user.id, "admin", (err) => {
+          if (err) {
+            return next(err);
+          }
+          res.redirect("/");
+        });
+      else res.redirect("/");
+    }
+  );
 };
