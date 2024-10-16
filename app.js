8d89e944174c233ad4bf0149d7354cc19d99d648
Alex Erdei <mralexerdei@yahoo.co.uk>
2024-10-16 02:59:09 +0100

    Optimize CSS and JS loading

--- app.js
+++ app.js
@@ -22,26 +22,30 @@ var aboutRouter = require("./routes/about");
 var app = express();
 
 app.use(
-	helmet({
-		contentSecurityPolicy: {
-			directives: {
-				"frame-ancestors": ["'self'", "https://www.youtube-nocookie.com/"],
-				frameSrc: ["'self'", "https://www.youtube-nocookie.com/"],
-				childSrc: ["'self'", "https://www.youtube-nocookie.com/"],
-				"img-src": ["'self'", "https://i.ytimg.com/"],
-				"script-src": ["'self'", "https://www.youtube.com/"],
-			},
-			//reportOnly: true,
-		},
-		referrerPolicy: false,
-		originAgentCluster: false,
-	})
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        "frame-ancestors": ["'self'", "https://www.youtube-nocookie.com/"],
+        frameSrc: ["'self'", "https://www.youtube-nocookie.com/"],
+        childSrc: ["'self'", "https://www.youtube-nocookie.com/"],
+        "img-src": ["'self'", "https://i.ytimg.com/"],
+        "script-src": [
+          "'self'",
+          "https://www.youtube.com/",
+          "'sha256-pLrwPAx6VnbIKVIjRVyGAqfm1Xhf37GDZKZjsubPP98='",
+        ],
+      },
+      //reportOnly: true,
+    },
+    referrerPolicy: false,
+    originAgentCluster: false,
+  })
 );
 
 //Without this middleware embedded youtube video is not working with helmet
 app.use((req, res, next) => {
-	res.header("Cross-Origin-Embedder-Policy", "cross-origin");
-	next();
+  res.header("Cross-Origin-Embedder-Policy", "cross-origin");
+  next();
 });
 
 // Set up mongoose connection
@@ -61,47 +65,47 @@ app.use(express.urlencoded({ extended: false }));
 
 // user authentication
 passport.use(
-	new LocalStrategy((username, password, done) => {
-		User.findOne({ user_name: username }, (err, user) => {
-			if (err) {
-				return done(err);
-			}
-			if (!user) {
-				return done(null, false, { message: "Incorrect username" });
-			}
-			bcrypt.compare(password, user.hash, (err, res) => {
-				if (res) {
-					// passwords match! log user in
-					return done(null, user);
-				} else {
-					// passwords do not match!
-					return done(null, false, { message: "Incorrect password" });
-				}
-			});
-		});
-	})
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ user_name: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      bcrypt.compare(password, user.hash, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
+    });
+  })
 );
 
 passport.serializeUser(function (user, done) {
-	done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
-	User.findById(id, function (err, user) {
-		done(err, user);
-	});
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
 });
 
 //app.use(cookieParser());
 app.use(
-	session({
-		secret: process.env.SECRET,
-		resave: false,
-		saveUninitialized: true,
-		store: MongoStore.create({
-			mongoUrl: mongoDB,
-		}),
-	})
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: mongoDB,
+    }),
+  })
 );
 app.use(passport.initialize());
 app.use(passport.session());
@@ -112,14 +116,14 @@ app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-	res.locals.currentUser = req.user;
-	const errorMessages = req.flash().error;
-	res.locals.errors = errorMessages
-		? errorMessages.map((error) => {
-				return { msg: error };
-		  })
-		: [];
-	next();
+  res.locals.currentUser = req.user;
+  const errorMessages = req.flash().error;
+  res.locals.errors = errorMessages
+    ? errorMessages.map((error) => {
+        return { msg: error };
+      })
+    : [];
+  next();
 });
 
 app.use("/", indexRouter);
@@ -128,28 +132,28 @@ app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
 
 app.post(
-	"/users/login",
-	passport.authenticate("local", {
-		successRedirect: "/posts",
-		failureRedirect: "/users/login",
-		failureFlash: true,
-	})
+  "/users/login",
+  passport.authenticate("local", {
+    successRedirect: "/posts",
+    failureRedirect: "/users/login",
+    failureFlash: true,
+  })
 );
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-	next(createError(404));
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get("env") === "development" ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render("error");
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
